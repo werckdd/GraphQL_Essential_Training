881ea02dd6fd0aa79e8b11b1544935de58c8ce46
werckdd <lamerialChan@gmail.com>
2018-03-07 16:17:16 +0800

    add_resolver

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import graphqlHTTP from "express-graphql";
 import schema from "./schema";
+import resolvers from './resolvers';
 
 const app = express();
 
@@ -8,41 +9,9 @@ app.get("/", (req, res) => {
   res.send("GraphQL is amazing");
 });
 
-class Friend {
-	constructor(id, {firstName,lastName,gender,language,email}){
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.gender = gender;
-		this.email = email;
-	}
-}
-
-const root = {
-  friend: () => {
-    return {
-      id: 28931238,
-      firstName: "Lamerial",
-      lastName: "Qian",
-      gender: "Male",
-      language: "Chinese",
-      emails: [{ email: "me@me.com" }, { email: "another@me.com" }],
-    }
-  },
-	createFriend: ({input}) => {
-		let id = require('crypto').randomBytes(10).toString('hex');
-	//	friendDatabase[id] = input;
-		return new Friend(id, input);
-	}
-};
-
 app.use(
   "/graphql",
-  graphqlHTTP({
-    schema,
-    rootValue: root,
-    graphiql: true
-  })
+	graphqlHTTP({ schema, rootValue: resolvers, graphiql: true })
 );
 
 app.listen(8080, () =>
